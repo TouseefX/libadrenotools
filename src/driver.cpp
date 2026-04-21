d56e4efcb021b16889e080bc798cd37ef5faa308
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-04-21 22:04:40 +0400

    Try to fix kernel panic because of screen tearing and no errors reporting

--- src/driver.cpp
+++ src/driver.cpp
@@ -502,10 +502,10 @@ static void global_atomic_init() {
     setenv("MESA_VULKAN_ICD_SELECT", "turnip", 1);
     setenv("MESA_VK_IGNORE_CONFORMANCE_WARNING", "true", 1);
     setenv("MESA_VK_DEVICE_SELECT_FORCE_DEFAULT_DEVICE", "1", 1);
-	setenv("MESA_VK_WSI_PRESENT_MODE", "immediate", 1);
-    setenv("MESA_NO_ERROR", "1", 1);
+	setenv("MESA_VK_WSI_PRESENT_MODE", "mailbox", 1);
+    setenv("MESA_NO_ERROR", "0", 1);
 	setenv("MESA_GLSL_CACHE_DISABLE", "false", 1);
-    setenv("MESA_GLSL_CACHE_MAX_SIZE", "256M", 1);
+    setenv("MESA_GLSL_CACHE_MAX_SIZE", "1G", 1);
 	setenv("ADRENO_TURBO", "1", 1);
 	setenv("KGSL_CONTEXT_PRIORITY", "1", 1);
     setenv("FD_DEV_FEATURES", "enable_tp_ubwc_flag_hint=1", 1);
