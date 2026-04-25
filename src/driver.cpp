f7c657c654e345f11c6155df059fe730f1f99921
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-04-25 14:43:15 +0400

    Update driver.cpp

--- src/driver.cpp
+++ src/driver.cpp
@@ -291,9 +291,7 @@ static void* hooked_android_dlopen_ext(
         }
     }
 
-    if (filename && (strstr(filename, "libvulkan.so") ||
-                     strstr(filename, "vulkan.adreno.so") ||
-                     strstr(filename, "vulkan.msm8998.so"))) {
+    if (filename && strstr(filename, "vulkan") && strstr(filename, ".so")) {
         ALOGI("android_dlopen_ext intercepted: %s → Turnip", filename);
         return g_turnip_handle;
     }
@@ -314,9 +312,7 @@ static void* hooked_dlopen(const char* filename, int flags) {
         }
 	}
 
-	if (filename && (strstr(filename, "libvulkan.so") ||
-                     strstr(filename, "vulkan.adreno.so") ||
-                     strstr(filename, "vulkan.msm8998.so"))) {
+	if (filename && strstr(filename, "vulkan") && strstr(filename, ".so")) {
         ALOGI("dlopen intercepted: %s → Turnip", filename);
         return g_turnip_handle;
 	}
