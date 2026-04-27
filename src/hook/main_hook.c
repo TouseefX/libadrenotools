41ba48640fae27dd8e1159ec859db9e923df2a77
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-04-27 14:26:58 +0400

    Add system properly hook

--- src/hook/main_hook.c
+++ src/hook/main_hook.c
@@ -1,9 +1,18 @@
 #include "hook_impl.h"
+#include <sys/system_properties.h>
 
 __attribute__((visibility("default"))) void *android_dlopen_ext(const char *filename, int flags, const android_dlextinfo *extinfo) {
-	return hook_android_dlopen_ext(filename, flags, extinfo);
+    return hook_android_dlopen_ext(filename, flags, extinfo);
 }
 
 __attribute__((visibility("default"))) void *android_load_sphal_library(const char *filename, int flags) {
-	return hook_android_load_sphal_library(filename, flags);
+    return hook_android_load_sphal_library(filename, flags);
+}
+
+__attribute__((visibility("default"))) int __system_property_get(const char *name, char *value) {
+    return hook___system_property_get(name, value);
+}
+
+__attribute__((visibility("default"))) void __system_property_read_callback(const prop_info *pi, void (*callback)(void *, const char *, const char *, uint32_t), void *cookie) {
+    hook___system_property_read_callback(pi, callback, cookie);
 }
