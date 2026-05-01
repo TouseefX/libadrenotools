5b3e6bdaf8eb0cfe8d477c9c71faa5d4510ce65c
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-05-01 08:30:41 +0400

    Update driver.cpp

--- src/driver.cpp
+++ src/driver.cpp
@@ -399,7 +399,7 @@ void applyTurnipOptimizations() {
 				    setenv("TU_DEBUG", "sysmem,noconfirm,noflushall,lowprecision", 1);
 				#endif
             } else {
-                setenv("TU_DEBUG", "gmem,noconfirm,noflushall,lowprecision", 1);
+                setenv("TU_DEBUG", "sysmem,noconfirm,noflushall,lowprecision", 1);
                 ALOGI("Use System Memory");
             }
         }
